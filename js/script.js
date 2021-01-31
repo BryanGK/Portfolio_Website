5d75ed121c0004097b4ec7a9aa1e23c9102381d1
BryanGK <bryangordonkrauss@gmail.com>
2021-01-31 06:46:51 -0800

    Working adding some jQuery eventlisteners and smooth scrolling. moved jQuery from html to script.js

---
+++ js/script.js
@@ -0,0 +1,27 @@
+!function (d, s, id) { var js, fjs = d.getElementsByTagName(s)[0], p = /^http:/.test(d.location) ? 'http' : 'https'; if (!d.getElementById(id)) { js = d.createElement(s); js.id = id; js.src = p + '://platform.twitter.com/widgets.js'; fjs.parentNode.insertBefore(js, fjs); } }(document, 'script', 'twitter-wjs');
+
+$(document).ready(function () {
+    // Smooth scrolling
+    var $root = $('html, body');
+    $('.navbar-nav a').click(function () {
+        var href = $.attr(this, 'href');
+        $root.animate({
+            scrollTop: $(href).offset().top
+        }, 500, function () {
+            window.location.hash = href;
+        });
+        return false;
+    });
+
+    // Tooltips
+    $(function () {
+        $('[data-toggle="tooltip"]').tooltip();
+    });
+
+    // Button Event Handler
+    $("#submit").on('click', function () {
+        var comment = $('.message-box').val();
+        console.log(comment);
+        return false;
+    });
+});
