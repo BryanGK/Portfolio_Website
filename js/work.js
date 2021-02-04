0ef89a6f2b283cd3c6873b3deec8ff94d5bd9843
BryanGK <bryangordonkrauss@gmail.com>
2021-02-03 18:52:02 -0800

    Woking thru CareerFoundry Web Dev course.

--- js/work.js
+++ js/work.js
@@ -1,4 +1,23 @@
 const works = [
-    "/img/road.jpg", "/img/road.jpg", "/img/road.jpg",
     "/img/road.jpg", "/img/road.jpg", "/img/road.jpg"
 ];
+const works2 = [
+    "/img/road.jpg", "/img/road.jpg", "/img/road.jpg"
+];
+
+for (let i = 0; i < works.length; i++) {
+    $("#work").append("\
+  <div class='col'>\
+    <img class='col proj-img' src='" + works[i] + "' class='img - fluid'>\
+  </div>\
+");
+};
+
+for (let i = 0; i < works.length; i++) {
+    $("#work2").append("\
+  <div class='col'>\
+    <img class='col proj-img' src='" + works2[i] + "' class='img - fluid'>\
+  </div>\
+");
+
+};
