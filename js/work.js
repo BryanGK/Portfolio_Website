3985a270b1fd729f9ef07522e6b16fe8c8722fae
BryanGK <bryangordonkrauss@gmail.com>
2021-02-05 15:17:33 -0800

    Adding links and covers to my projects

--- js/work.js
+++ js/work.js
@@ -1,23 +1,37 @@
+// const works = [
+//     "/img/road.jpg", "/img/road.jpg", "/img/road.jpg"
+// ];
+// const works2 = [
+//     "/img/road.jpg", "/img/road.jpg", "/img/road.jpg"
+// ];
+
 const works = [
-    "/img/road.jpg", "/img/road.jpg", "/img/road.jpg"
-];
-const works2 = [
-    "/img/road.jpg", "/img/road.jpg", "/img/road.jpg"
+  {
+    title: "First Project",
+    pic: "/img/road.jpg",
+  },
+  {
+    title: "Second Project",
+    pic: "/img/road.jpg",
+  },
+  {
+    title: "Third Project",
+    pic: "/img/road.jpg",
+  },
+  {
+    title: "Fourth Project",
+    pic: "/img/road.jpg",
+  },
 ];
 
 for (let i = 0; i < works.length; i++) {
-    $("#work").append("\
+  $("#work").append("\
   <div class='col'>\
-    <img class='col proj-img' src='" + works[i] + "' class='img - fluid'>\
+    <a href='#' class='work-img'>\
+      <img class='col proj-img' src='" + works[i].pic + "'>\
+      <span class='info'><p class='proj-title'>Title:</p> [WORK TITLE] </span>\
+    </a>\
   </div>\
 ");
 };
 
-for (let i = 0; i < works.length; i++) {
-    $("#work2").append("\
-  <div class='col'>\
-    <img class='col proj-img' src='" + works2[i] + "' class='img - fluid'>\
-  </div>\
-");
-
-};
